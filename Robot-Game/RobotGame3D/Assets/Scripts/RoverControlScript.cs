c1117366e4d9e0839a62eb4ba2e853ebdcfe7b18
Ryan-Afzal <ryan.afzal@outlook.com>
2019-07-18 16:07:57 -0700

    Made a rover bot, and experimented with the Unity joint system

---
+++ Robot-Game/RobotGame3D/Assets/Scripts/RoverControlScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverControlScript : MonoBehaviour {
+
+	public HingeJoint[] leftWheels;
+	public HingeJoint[] rightWheels;
+
+	public int speed;
+	public int maxSpeed;
+
+	private void Start() {
+		foreach (var j in leftWheels) {
+			j.useMotor = true;
+		}
+		
+		foreach (var j in rightWheels) {
+			j.useMotor = true;
+		}
+	}
+
+	private void Update() {
+
+	}
+}
