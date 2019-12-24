d98b7664d7218ee1af46bd40e1cceb526de5384d
Ryan-Afzal <ryan.afzal@outlook.com>
2019-12-23 22:56:49 -0800

    Updated to new unity version
    Also resumed work on instructions

---
+++ Robot-Game/RobotGame3D/Assets/Scripts/Scripting/IInstruction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Scripting {
+	/// <summary>
+	/// Base type for instructions.
+	/// </summary>
+	public interface IInstruction {
+
+		/// <summary>
+		/// The previous instruction in the expression tree.
+		/// </summary>
+		public IInstruction Previous { get; set; }
+
+		/// <summary>
+		/// The next instruction in the expression tree.
+		/// </summary>
+		public IInstruction Next { get; set; }
+
+		/// <summary>
+		/// Runs just before the instruction executes the first time.
+		/// </summary>
+		void Begin();
+
+		/// <summary>
+		/// Runs each update during execution. It returns whether the instruction has finished executing.
+		/// </summary>
+		/// <returns>Returns whether the instruction is finished executing.</returns>
+		bool Update();
+
+		/// <summary>
+		/// Runs after the instruction is finished executing, when <c>Update()</c> returns <c>true</c>.
+		/// </summary>
+		void End();
+	}
+}
--- Robot-Game/RobotGame3D/Assets/Scripts/Scripting/NestedInstruction.cs
+++ Robot-Game/RobotGame3D/Assets/Scripts/Scripting/NestedInstruction.cs
@@ -1,24 +1,46 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 
 namespace Assets.Scripts.Scripting {
+	/// <summary>
+	/// Base type for nested instructions, which have separate expression tree branches inside of them.
+	/// </summary>
+	public abstract class NestedInstruction : IInstruction {
 
-	public abstract class NestedInstruction : StandaloneInstruction {
+		/// <summary>
+		/// The instructions nested inside.
+		/// </summary>
+		private readonly IInstruction[] instructions;
 
-		public int numInstructions;
-
-		private StandaloneInstruction[] instructions;
-
-		protected override void Awake() {
-			base.Awake();
-
-			this.instructions = new StandaloneInstruction[this.numInstructions];
+		public NestedInstruction(IInstruction[] instructions) {
+			this.instructions = instructions;
 		}
 
+		public IInstruction this[int i] {
+			get {
+				return instructions[i];
+			}
+		}
+
+		/// <summary>
+		/// The number of different expression tree branches nested inside this instruction.
+		/// </summary>
+		public int Count {
+			get {
+				return this.instructions.Length;
+			}
+		}
+
+		public abstract IInstruction Previous { get; set; }
+		public abstract IInstruction Next { get; set; }
+
+		public abstract void Begin();
+
+		public abstract bool Update();
+
+		public abstract void End();
 	}
 }
