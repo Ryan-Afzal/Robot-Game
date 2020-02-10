8431c5c8447b72a5819b14658680448ef87e2a39
Ryan-Afzal <ryan.afzal@outlook.com>
2020-02-10 14:02:16 -0800

    Update BlockInstruction.cs

--- Robot-Game/RobotGame3D/Assets/Scripts/Scripting/Block/BlockInstruction.cs
+++ Robot-Game/RobotGame3D/Assets/Scripts/Scripting/Block/BlockInstruction.cs
@@ -1,1 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
 
+namespace Assets.Scripts.Scripting.Block {
+
+  public abstract class BlockInstruction : MonoBehaviour {
+    
+    private string startText;
+    private string[] text;
+    private IArgInstruction[] args;
+    
+    protected void Init(string startText, params string[] text) {
+      this.startText = startText;
+      this.text = text;
+      this.args = new IArgInstruction[this.text.Length];
+    }
+    
+    public BlockInstruction Previous { get; private set; }
+    public BlockInstruction Next { get; private set; }
+    
+    public abstract IInstruction GetCompiledValue();
+    
+  }
+  
+}
