174b93404ac3b3897d091e4aca0f00a7c9f6c19a
Ryan-Afzal <ryan.afzal@outlook.com>
2020-02-11 08:49:05 -0800

    Create TrashSlot.cs

---
+++ Robot-Game/RobotGame3D/Assets/Scripts/Scripting/Block/TrashSlot.cs
@@ -0,0 +1,15 @@
+using Assets.Scripts.Scripting.Compiled;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Scripting.Block {
+    public class TrashSlot : MonoBehaviour {
+        
+    }
+}
