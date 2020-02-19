04c7391aabdaa893fa0bcf71d94b7f6bde7843f1
Ryan-Afzal <ryan.afzal@outlook.com>
2020-02-19 10:10:35 -0800

    Added arguments
    Next step is to implement drawing text and relative positioning

---
+++ Robot-Game/RobotGame3D/Assets/Scripts/Scripting/Block/ArgSocket.cs
@@ -0,0 +1,51 @@
+﻿using Assets.Scripts.Scripting.Compiled;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Scripting.Block {
+	public sealed class ArgSocket : MonoBehaviour {
+
+		private RectTransform rectTransform;
+		private Image image;
+		private Shadow shadow;
+
+		private bool visible;
+
+		public bool Visible {
+			
+			get {
+				return this.visible;
+			}
+
+			set {
+				this.visible = value;
+				this.image.enabled = value;
+			}
+
+		}
+
+		public IHierarchyChangeHandler Base { get; set; }
+		public BlockArgument Argument { get; set; }
+
+		public void Awake() {
+			this.rectTransform = GetComponent<RectTransform>();
+			this.image = GetComponent<Image>();
+			this.shadow = GetComponent<Shadow>();
+		}
+
+		public void Update() {
+			this.shadow.enabled = Visible ? RectTransformUtility.RectangleContainsScreenPoint(this.rectTransform, Input.mousePosition) : false;
+		}
+
+		public void HierarchyChanged() {
+			Argument.HierarchyChanged();
+		}
+
+	}
+}
--- Robot-Game/RobotGame3D/Assets/Scripts/Scripting/Block/BlockArgument.cs
+++ Robot-Game/RobotGame3D/Assets/Scripts/Scripting/Block/BlockArgument.cs
@@ -1,4 +1,4 @@
-using Assets.Scripts.Scripting.Compiled;
+﻿using Assets.Scripts.Scripting.Compiled;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,42 +9,24 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.Scripting.Block {
+	public abstract class BlockArgument : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IHierarchyChangeHandler {
 
-    public abstract class BlockInstruction : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IHierarchyChangeHandler {
+		private Image image;
+		private Shadow shadow;
+		private RectTransform rectTransform;
 
-        private ArgSocket[] argSockets;
-
-        private Image image;
-        private Shadow shadow;
-        private RectTransform rectTransform;
-
-        public string startText;
-        public string[] text;
-
-        public Vector3 offset = new Vector3(0, -100, 0);
+        public Vector3 offset = new Vector3(0, 0, 0);
 
         public Canvas canvas;
 
-        public GameObject argSocketPrefab;
-
         public bool Locked { get; set; }
 
-        public BlockInstruction Previous { get; set; }
-        public BlockInstruction Next { get; set; }
+        public ArgSocket Base { get; set; }
 
         public void Awake() {
             this.rectTransform = GetComponent<RectTransform>();
             this.image = GetComponent<Image>();
             this.shadow = GetComponent<Shadow>();
-
-            this.argSockets = new ArgSocket[this.text.Length];
-            
-            for (int i = 0; i < this.argSockets.Length; i++) {
-                this.argSockets[i] = Instantiate(this.argSocketPrefab, this.rectTransform).GetComponent<ArgSocket>();
-                this.argSockets[i].Base = this;
-
-                // Set Position
-            }
         }
 
         public virtual void Start() {
@@ -56,13 +38,7 @@ namespace Assets.Scripts.Scripting.Block {
         }
 
         public virtual void HierarchyChanged() {
-            if (Next is object) {
-                Next.HierarchyChanged();
-            }
-
-            for (int i = 0; i < this.argSockets.Length; i++) {
-                this.argSockets[i].HierarchyChanged();
-            }
+            // Call on arguments
         }
 
         private void SetColorDrag() {
@@ -77,9 +53,9 @@ namespace Assets.Scripts.Scripting.Block {
             if (!Locked) {
                 this.rectTransform.SetParent(this.canvas.transform, true);
 
-                if (Previous is object) {
-                    Previous.Next = null;
-                    Previous = null;
+                if (Base is object) {
+                    Base.Argument = null;
+                    Base = null;
                 }
 
                 SetColorDrag();
@@ -106,25 +82,24 @@ namespace Assets.Scripts.Scripting.Block {
                     return;
                 }
 
-                var obj = results.FirstOrDefault(o => o.GetComponent<BlockInstruction>() is object);
+                var obj = results.FirstOrDefault(o => o.GetComponent<ArgSocket>() is object);
 
                 if (obj is object) {
-                    var result = obj.GetComponent<BlockInstruction>();
+                    var result = obj.GetComponent<ArgSocket>();
                     var resultTransform = result.GetComponent<RectTransform>();
 
                     this.rectTransform.position = resultTransform.position + this.offset;
                     this.rectTransform.SetParent(resultTransform, true);
 
-                    Previous = result;
-                    Previous.Next = this;
+                    Base = result;
+                    Base.Argument = this;
                 }
 
                 SetColorEndDrag();
             }
         }
 
-        public abstract IInstruction GetCompiledInstruction();
+        public abstract IArgInstruction GetCompiledInstruction();
 
     }
-
 }
