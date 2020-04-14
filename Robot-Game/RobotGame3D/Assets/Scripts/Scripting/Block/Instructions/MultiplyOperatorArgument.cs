200b42a012656e4c2dcfec83de0e72489f7015d6
Ryan-Afzal <ryan.afzal@outlook.com>
2020-04-14 11:18:22 -0700

    tried to add the robot arm, and added math operators

---
+++ Robot-Game/RobotGame3D/Assets/Scripts/Scripting/Block/Instructions/MultiplyOperatorArgument.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Scripting.Compiled;
+using Assets.Scripts.Scripting.Compiled.Instructions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Scripting.Block.Instructions {
+	public sealed class MultiplyOperatorArgument : OperatorArgument {
+		protected override IArgInstruction Compile() {
+			return new MultiplyOperator(new IArgInstruction[] { this.argSockets[0].AttachedArgument.GetCompiledArgument(), this.argSockets[1].AttachedArgument.GetCompiledArgument() });
+		}
+
+		protected override string GetName() {
+			return "*";
+		}
+	}
+}
